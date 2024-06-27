029c4357e87ed41e9529dfee1c7a33d886880060
lakshmi-git123 <haiprasu8794@gmail.com>
2024-06-27 10:22:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,39 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = "${var.project_id}-storage-bucket"
+  location      = var.region
+  force_destroy = true
+}
+
+resource "google_firestore_index" "index" {
+  # Example Firestore index configuration
+}
+
+resource "google_cloud_run_service" "service" {
+  name     = "DI-py-api"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project_id}/DI-py-api"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+output "cloud_run_url" {
+  value = google_cloud_run_service.service.status[0].url
+}
+
+
 
